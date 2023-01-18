c1b48ba85f77ca8fb23f538e631d6b45e8018a70
raghav khandelwal <specialyoufullstack@gmail.com>
2023-01-18 16:49:30 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialArray = [
+  { id: 1, balloon: "dark" },
+  { id: 2, balloon: "light" },
+  { id: 3, balloon: "color" },
+];
+
+const style = {
+  color: "red",
+  backgroundColor: "black",
+};
 
 const App = () => {
-  return <div>raghav khandelwal</div>;
+  const updateArray = initialArray;
+  const [show, setShow] = useState(true);
+  console.log(show);
+  return (
+    <>
+      <p
+        onClick={() => {
+          setShow(false);
+        }}
+      >
+        there are {updateArray.length} balloons
+      </p>
+      {updateArray.map((updateArr) => (
+        <ListSubapp
+          key={updateArr.id}
+          updateArr={updateArr}
+          style={style}
+          propass="text"
+        />
+      ))}
+    </>
+  );
 };
+
+const ListSubapp = ({ updateArr, style }) => {
+  return (
+    <>
+      <li style={style}>{updateArr.balloon}</li>
+    </>
+  );
+};
+
+// const App = () => {
+//   const updateArray = initialArray;
+//   return (
+//     <>
+//       {updateArray.map((updateArr) => (
+//         <li key={updateArr.id} style={style}>
+//           {updateArr.balloon}
+//         </li>
+//       ))}
+//     </>
+//   );
+// };
 
 export default App;
