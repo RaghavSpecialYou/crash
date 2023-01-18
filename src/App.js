b364fce017957c452f9dadc82bfd33179e55a4d0
raghav khandelwal <specialyoufullstack@gmail.com>
2023-01-18 17:57:05 +0530

    first commit of raghav1 branch

--- src/App.js
+++ src/App.js
@@ -1,60 +1,7 @@
-import React, { useState } from "react";
-
-const initialArray = [
-  { id: 1, balloon: "dark" },
-  { id: 2, balloon: "light" },
-  { id: 3, balloon: "color" },
-];
-
-const style = {
-  color: "red",
-  backgroundColor: "black",
-};
+import React from "react";
 
 const App = () => {
-  const updateArray = initialArray;
-  const [show, setShow] = useState(true);
-  console.log(show);
-  return (
-    <>
-      <p
-        onClick={() => {
-          setShow(false);
-        }}
-      >
-        there are {updateArray.length} balloons
-      </p>
-      {updateArray.map((updateArr) => (
-        <ListSubapp
-          key={updateArr.id}
-          updateArr={updateArr}
-          style={style}
-          propass="text"
-        />
-      ))}
-    </>
-  );
+  return <div>this is raghav1 branch</div>;
 };
-
-const ListSubapp = ({ updateArr, style }) => {
-  return (
-    <>
-      <li style={style}>{updateArr.balloon}</li>
-    </>
-  );
-};
-
-// const App = () => {
-//   const updateArray = initialArray;
-//   return (
-//     <>
-//       {updateArray.map((updateArr) => (
-//         <li key={updateArr.id} style={style}>
-//           {updateArr.balloon}
-//         </li>
-//       ))}
-//     </>
-//   );
-// };
 
 export default App;
