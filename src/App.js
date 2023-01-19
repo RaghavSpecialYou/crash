c9e3c461289bfa60239be7e38e26ab20eda8e2ff
raghav khandelwal <specialyoufullstack@gmail.com>
2023-01-19 10:11:06 +0530

    this is third commit of local branch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ const App = () => {
       <div>this is raghav1 branch</div>
       <div>
         I am doing second commit in this local branch but will not push it to
-        the remote repo
+        the remote repo this is third commit in this local branch and will push
+        it to the remote repo.
       </div>
     </>
   );
