3a993260a430196ff07fe767ff110e1efaee2169
raghav khandelwal <specialyoufullstack@gmail.com>
2023-01-19 10:09:13 +0530

    this is second commitof local branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const App = () => {
-  return <div>this is raghav1 branch</div>;
+  return (
+    <>
+      <div>this is raghav1 branch</div>
+      <div>
+        I am doing second commit in this local branch but will not push it to
+        the remote repo
+      </div>
+    </>
+  );
 };
 
 export default App;
